65e4ba209c88acc6e8438e050f0563e12c0d2089
Adam Britt <ABritt@ritehite.com>
2023-10-19 10:41:47 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const overview = document.querySelector(".overview"); // div containing profile info
+const username = "adam-britt"; // username
+
+const getUserInfo = async function() {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    console.log(data);
+    displayUserInfo(data);
+};
+
+getUserInfo();
+
+const displayUserInfo = function(data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = 
+        `<figure>
+            <img alt="user avatar" src=${} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${}</p>
+            <p><strong>Bio:</strong> ${}</p>
+            <p><strong>Location:</strong> ${}</p>
+            <p><strong>Number of public repos:</strong> ${}</p>
+        </div>`
+};
+
