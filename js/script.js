bee101412c508a9e070d7b43865f7a4a4f68adbb
Adam Britt <ABritt@ritehite.com>
2023-10-19 11:04:26 -0500

    added user info to overview div

--- js/script.js
+++ js/script.js
@@ -13,17 +13,17 @@ getUserInfo();
 const displayUserInfo = function(data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
-    div.innerHTML = 
-        `<figure>
-            <img alt="user avatar" src=${} />
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatarURL} />
         </figure>
         <div>
-            <p><strong>Name:</strong> ${}</p>
-            <p><strong>Bio:</strong> ${}</p>
-            <p><strong>Location:</strong> ${}</p>
-            <p><strong>Number of public repos:</strong> ${}</p>
-        </div>`
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+    `;
+    overview.append(div);
 };
-
-<div></div>
 
